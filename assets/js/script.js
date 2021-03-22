551f12c70f9a8d8be49c5ee5be744569e53410c2
Roselyn Le <roselynle95@gmail.com>
2021-03-22 22:50:04 +0000

    function for announcing winner and restarting game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 const choices = document.querySelectorAll(".choice");
+const score = document.getElementById("score");
+const result = document.getElementById("result");
+const modal = document.querySelector(".modal");
+const scoreboard = {
+    player: 0,
+    computer: 0,
+};
 
 // Event listener for player choice
 choices.forEach((choice) => choice.addEventListener("click", play));
@@ -34,4 +41,49 @@ function playRound(playerSelection, computerSelection) {
             return "player";
         }
     }
+}
+
+function announceWinner(winner, computerChoice) {
+    if (winner === "player") {
+        // Increment player score
+        scoreboard.player++;
+        // Show modal result
+        result.innerHTML = `<h1 class="text-win">You Win</h1>
+      <i class="fas fa-hand-${computerChoice} fa-8x"></i>
+      <p>Computer Chose <b>${
+          computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</b></p>`;
+
+        if (scoreboard.player === 5) {
+            restart.addEventListener("click", restartGame);
+        }
+    } else if (winner === "computer") {
+        // Increment computer score
+        scoreboard.computer++;
+        // Show modal result
+        result.innerHTML = `<h1 class="text-lose">You Lose</h1>
+      <i class="fas fa-hand-${computerChoice} fa-8x"></i>
+      <p>Computer Chose <b>${
+          computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</b></p>`;
+    } else {
+        result.innerHTML = `<h1>It's a Draw</h1>
+      <i class="fas fa-hand-${computerChoice} fa-8x"></i>
+      <p>Computer Chose <b>${
+          computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)
+      }</b></p>`;
+    }
+    // Show score
+    score.innerHTML = `<p>Player: ${scoreboard.player}</p>
+  <p>Computer: ${scoreboard.computer}</p>`;
+
+    modal.style.display = "block";
+}
+
+// Restart game
+function restartGame() {
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `<p>Player: 0</p>
+  <p>Computer: 0</p>`;
 }
